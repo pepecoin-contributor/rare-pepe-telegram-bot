57a75b34c7643333808a438f69e987140b862663
pepecoin-contributor <pepecoin-contributor@protonmail.com>
2021-02-04 00:06:24 -0500

    Updated bot

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-//hello world
-console.log("hello world");
+const TelegramBot = require('node-telegram-bot-api');
+const pepes = require('rare-pepe-api');
+
+require('dotenv').config();
+
+const token = process.env.TELEGRAM_TOKEN;
+let bot;
+
+if (process.env.NODE_ENV === 'production') {
+  bot = new TelegramBot(token);
+  bot.setWebHook(process.env.HEROKU_URL + bot.token);
+} else {
+  bot = new TelegramBot(token, { polling: true });
+}
+
+// Matches "/random"
+bot.onText(/\/random/, (msg) => {
+  const chatId = msg.chat.id;
+  const random = pepes.getRandom();
+  bot.sendMessage(chatId, random.img_url, { parse_mode: 'HTML' });
+});
